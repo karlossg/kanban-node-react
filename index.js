1497bf0f605210bf856b21faa5e49663199c1584
karlossg <karol.gratus@gmail.com>
2018-02-15 20:09:51 +0100

    passport start

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ test: 'server  ' });
-})
+
 
 
 const PORT = process.env.PORT || 5000;

7385debc2c62c3efa97b493d74edf6052bcf4cf6
karlossg <karol.gratus@gmail.com>
2018-02-16 20:38:04 +0100

    logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,6 +8,8 @@ module.exports = app => {
     })
   );
 
+  app.get('/api/logout', (req, res) => {});
+
   app.get('/auth/google/callback', passport.authenticate('google'));
 
   app.get('/api/current_user', (req, res) => {

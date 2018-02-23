082d0768b9c1f683df80470195876605b04090ec
Karol <karol.gratus@gmail.com>
2018-02-23 14:53:30 +0100

    react redux connect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import connect from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header'
 
@@ -7,19 +9,26 @@ const Landing = () => <h2> Landing </h2>
 const Dashboard = () => <h2> Dashboard </h2>
 const KanbanNew = () => <h2> KanbanNew </h2>
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/boards' component={Dashboard} />
-          <Route path='/boards/new' component={KanbanNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container" >
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/boards' component={Dashboard} />
+            <Route path='/boards/new' component={KanbanNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App
+const test = connect(null, actions)(App)
+
+export { test };
--- client/src/reducers/actions/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-import { FETCH_USER } from './types';
-
-const fetchUser = () => {
-  axios.get('/api/current_user');
-}

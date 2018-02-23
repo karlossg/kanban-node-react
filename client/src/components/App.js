8b57655308a3a4aab29110d4f96f473a8cecb783
Karol <karol.gratus@gmail.com>
2018-02-23 10:40:36 +0100

    materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2> Header </h2>
+import Header from './Header'
+
 const Landing = () => <h2> Landing </h2>
 const Dashboard = () => <h2> Dashboard </h2>
 const KanbanNew = () => <h2> KanbanNew </h2>
@@ -11,7 +12,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path='/' component={Header} />
+          <Header />
           <Route exact path='/' component={Landing} />
           <Route exact path='/boards' component={Dashboard} />
           <Route path='/boards/new' component={KanbanNew} />

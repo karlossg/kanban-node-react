69e1e1a936258909815235b19a5d29c0dfc800ee
Karol <karol.gratus@gmail.com>
2018-02-23 10:26:57 +0100

    react router first attemp

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2> Header </h2>
+const Landing = () => <h2> Landing </h2>
+const Dashboard = () => <h2> Dashboard </h2>
+const KanbanNew = () => <h2> KanbanNew </h2>
 
 const App = () => {
   return (
     <div>
-      Helloz kanban board!
+      <BrowserRouter>
+        <div>
+          <Route path='/' component={Header} />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/boards' component={Dashboard} />
+          <Route path='/boards/new' component={KanbanNew} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }

1aab808af5003a08f59de37acdc4326486110e43
Karol <karol.gratus@gmail.com>
2018-02-28 15:32:09 +0100

    logout route

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 const styleLogo = {
   marginLeft: '10px'
-}
+};
 
 class Header extends Component {
   renderOptions() {
@@ -11,24 +11,35 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return <li><a href="/google/auth" className="waves-effect light-blue btn">Login</a></li>
+        return (
+          <li>
+            <a href="/auth/google" className="waves-effect light-blue btn">
+              Login
+            </a>
+          </li>
+        );
       default:
-        return <li><a href="/api/logout" className="waves-effect light-blue btn">Logout</a></li>
+        return (
+          <li>
+            <a href="/api/logout" className="waves-effect light-blue btn">
+              Logout
+            </a>
+          </li>
+        );
     }
   }
 
   render() {
-    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper light-blue lighten-1">
-          <a className="left brand-logo" style={styleLogo}>KanbanIT</a>
-          <ul className="right">
-            {this.renderOptions()}
-          </ul>
+          <a className="left brand-logo" style={styleLogo}>
+            KanbanIT
+          </a>
+          <ul className="right">{this.renderOptions()}</ul>
         </div>
       </nav>
-    )
+    );
   }
 }
 

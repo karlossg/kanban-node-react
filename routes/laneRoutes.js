ddda7f526060b75d32945b15c3fbc1b55b39aa59
karlossg <karol.gratus@gmail.com>
2018-02-28 22:39:02 +0100

    ok

--- routes/laneRoutes.js
+++ routes/laneRoutes.js
@@ -1,6 +1,3 @@
-const _ = require('lodash');
-const Path = require('path-parser');
-const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 

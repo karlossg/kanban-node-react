ba571d368747cf3b7b6e8cc9833ad9ef6d5ac625
Karol <karol.gratus@gmail.com>
2018-03-01 15:28:07 +0100

    note reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
+import laneReducer from './laneReducer';
+import noteReducer from './noteReducer';
 
 export default combineReducers({
-  auth: authReducer
-})
+  auth: authReducer,
+  lane: laneReducer,
+  note: noteReducer
+});

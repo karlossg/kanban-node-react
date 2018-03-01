4d246b4d7c366cbb6fe7ec75a3a72cb3e08cbfc2
Karol <karol.gratus@gmail.com>
2018-03-01 14:58:04 +0100

    note actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import uuid from 'uuid-v4';
 
-import { FETCH_USER, CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './types';
+import {
+  FETCH_USER,
+  CREATE_LANE,
+  UPDATE_LANE,
+  DELETE_LANE,
+  CREATE_NOTE,
+  UPDATE_NOTE,
+  DELETE_NOTE
+} from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -29,6 +37,32 @@ export function updateLane(lane) {
 export function deleteLane(laneId) {
   return {
     type: DELETE_LANE,
+    laneId
+  };
+}
+
+export function createNote(note, laneId) {
+  return {
+    type: CREATE_NOTE,
+    laneId,
+    note: {
+      id: uuid(),
+      ...note
+    }
+  };
+}
+
+export function updateNote(note) {
+  return {
+    type: UPDATE_NOTE,
+    note
+  };
+}
+
+export function deleteNote(noteId, laneId) {
+  return {
+    type: DELETE_NOTE,
+    noteId,
     laneId
   };
 }

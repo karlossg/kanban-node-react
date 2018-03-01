0cbe9de6d7cd1a1c138088953f463a56526047f5
Karol <karol.gratus@gmail.com>
2018-03-01 15:10:39 +0100

    lane reducer

---
+++ client/src/reducers/laneReducer.js
@@ -0,0 +1,21 @@
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from '../actions/types';
+
+const initialState = [];
+
+export default function notes(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_NOTE:
+      return [...state, action.note];
+
+    case UPDATE_NOTE:
+      return state.map(note => {
+        return note.id === action.id ? { ...note, ...action.note } : note;
+      });
+
+    case DELETE_NOTE:
+      return state.filter(note => note.id !== action.noteId);
+
+    default:
+      return state;
+  }
+}
